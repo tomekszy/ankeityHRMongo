310c7e6166bd26e25759c3cd9d6c4b53a24d28db
Tomasz Szymański <tszymanski@mir.local>
2018-08-06 13:46:16 +0200

    drugi commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AnkietaComponent } from './components/ankieta/ankieta.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AnkietaComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/ankieta/ankieta.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ankieta',
+  templateUrl: './ankieta.component.html',
+  styleUrls: ['./ankieta.component.css']
+})
+export class AnkietaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
