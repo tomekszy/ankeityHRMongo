8b71be59eea905a2f1089ff15c5cac27e058e870
Tomasz Szymański <tszymanski@mir.local>
2018-08-06 13:47:07 +0200

    drugi commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyD2_5EqvqGFkpB3ehTYZCEHDhPmX0NGMV0',
+    authDomain: 'ankietyhr-a5489.firebaseapp.com',
+    databaseURL: 'https://ankietyhr-a5489.firebaseio.com',
+    projectId: 'ankietyhr-a5489',
+    storageBucket: 'ankietyhr-a5489.appspot.com',
+    messagingSenderId: '688130965724'
+  }
 };
